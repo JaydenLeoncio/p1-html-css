2d0eb7415e6c74b5f40c6f4f1f3db56acafed522
JaydenLeoncio <9024374@student.zadkine.nl>
2024-11-14 09:57:58 +0100

    test

--- Javamod/java.java
+++ Javamod/java.java
@@ -1,88 +1,41 @@
-package com.example.gunmod;
+@Mod("jujutsukaisenmod")
+public class JujutsuKaisenMod {
+    public static final String MOD_ID = "jujutsukaisenmod";
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.CreativeModeTabEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-import org.slf4j.Logger;
-
-@Mod(GunMod.MOD_ID)
-public class GunMod {
-    public static final String MOD_ID = "gunmod";
-    private static final Logger LOGGER = LogUtils.getLogger();
-
-    public GunMod() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        ItemsRegistry.ITEMS.register(modEventBus);
-
-        modEventBus.addListener(this::addCreative);
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void addCreative(CreativeModeTabEvent.BuildContents event) {
-        if (event.getTab() == CreativeModeTabs.COMBAT) {
-            event.accept(ItemsRegistry.GUN_ITEM);
-        }
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        LOGGER.info("Hello from server starting");
+    public JujutsuKaisenMod() {
+        // Code om items, entiteiten, en vaardigheden te registreren
     }
 }
-package com.example.gunmod.item;
-
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.entity.projectile.Arrow;
-
-public class GunItem extends Item {
-    public GunItem(Properties properties) {
-        super(properties);
+public class CursedDagger extends SwordItem {
+    public CursedDagger() {
+        super(Tiers.DIAMOND, 3, -2.4F, new Item.Properties().tab(CreativeModeTabs.TAB_COMBAT));
+    }
+}
+public class CursedEnergyAbility {
+    public static void activate(Level world, Player player) {
+        List<LivingEntity> entities = world.getEntitiesOfClass(LivingEntity.class, player.getBoundingBox().inflate(5));
+        for (LivingEntity entity : entities) {
+            if (entity != player) {
+                entity.hurt(DamageSource.MAGIC, 10.0F); // 10 punten schade
+                // Voeg speciale effecten toe, zoals particles
+            }
+        }
+    }
+}
+public class CursedSpirit extends Monster {
+    public CursedSpirit(EntityType<? extends Monster> type, Level world) {
+        super(type, world);
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
-        ItemStack itemstack = player.getItemInHand(hand);
-
-        // Maak en vuur een pijl af
-        if (!level.isClientSide) {
-            Arrow arrow = new Arrow(level, player);
-            arrow.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 3.0F, 1.0F);
-            level.addFreshEntity(arrow);
-            player.playSound(SoundEvents.CROSSBOW_SHOOT, 1.0F, 1.0F);
-        }
-
-        return InteractionResultHolder.sidedSuccess(itemstack, level.isClientSide());
+    protected void registerGoals() {
+        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, true));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, true));
     }
 }
-package com.example.gunmod;
-
-import com.example.gunmod.item.GunItem;
-import net.minecraft.world.item.Item;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-
-public class ItemsRegistry {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, GunMod.MOD_ID);
-
-    public static final RegistryObject<Item> GUN_ITEM = ITEMS.register("gun", 
-        () -> new GunItem(new Item.Properties().stacksTo(1).durability(100).tab(CreativeModeTabs.COMBAT)));
+public class DomainExpansion {
+    public static void activate(Player player) {
+        // Maak een domein met aangepaste regels en beperkingen
+        // Effecten zoals hogere schade, langzamere tijd, enz.
+    }
 }
